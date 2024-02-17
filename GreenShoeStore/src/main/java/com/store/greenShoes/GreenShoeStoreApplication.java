d00e16991a7bf6d7c93b6c5165db6493a098c38b
Sai Bharath Reddy Lattupalli <saibharathreddylattupalli@Sais-MacBook-Air.local>
2024-02-16 21:23:06 -0500

    initial setup

---
+++ GreenShoeStore/src/main/java/com/store/greenShoes/GreenShoeStoreApplication.java
@@ -0,0 +1,14 @@
+package com.store.greenShoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShoeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShoeStoreApplication.class, args);
+		System.out.println("GreenShoe");
+	}
+
+}
---
+++ GreenShoeStore/src/test/java/com/store/greenShoes/GreenShoeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.greenShoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShoeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
