a3b720a7f9062fb78d41d1ad081a94dfd6163f4b
Sai Bharath Reddy Lattupalli <saibharathreddylattupalli@Sais-MacBook-Air.local>
2024-02-27 01:14:51 -0500

    initial Customer Entities

--- GreenShoeStore/src/main/java/com/store/greenShoes/GreenShoeStoreApplication.java
+++ GreenShoeStore/src/main/java/com/store/greenShoes/GreenShoeStoreApplication.java
@@ -1,16 +1,14 @@
 package com.store.greenShoes;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class GreenShoeStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreenShoeStoreApplication.class, args);
-		System.out.print("HI");
-		
+		System.out.println("GreenShoe");
 	}
-	
 
 }
--- GreenShoeStore/src/main/java/com/store/greenShoes/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.greenShoes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.greenShoes.model.PaymentInformation;
-
-public interface PaymentRepository extends JpaRepository<PaymentInformation, Long>{
-
-}
