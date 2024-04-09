b1345a20446ca0dae2dccf639a29e9e93ef9603b
DESKTOP-UAC1IGU\harsh <harshagangari747@gmail.com>
2024-04-08 20:01:11 -0400

    Get order when given an order Id

--- GreenShoeStore/src/main/java/com/store/greenShoes/controller/OrderController.java
+++ GreenShoeStore/src/main/java/com/store/greenShoes/controller/OrderController.java
@@ -1,8 +1,9 @@
 package com.store.greenShoes.controller;
+
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,26 +17,39 @@ import com.store.greenShoes.repository.UsersRepository;
 import com.store.greenShoes.service.OrderService;
 
 @RestController
-public class OrderController{
+public class OrderController {
 	@Autowired
 	OrdersRepository orderRepo;
 	@Autowired
 	OrderService orderService;
 	@Autowired
 	UsersRepository userRepo;
+
 	@PostMapping("/order")
 	public OrderDTO postOrder(@RequestBody OrderDTO orderData) {
-		System.out.println("con"+orderData);
-		Users customer=null;
-		if(orderData.getUserId()!=null) {
-		customer=userRepo.getReferenceById(orderData.getUserId());}
-		return orderService.postorder(orderData,customer);
+		System.out.println("con" + orderData);
+		Users customer = null;
+		if (orderData.getUserId() != null) {
+			customer = userRepo.getReferenceById(orderData.getUserId());
+		}
+		return orderService.postorder(orderData, customer);
 	}
-	
+
 	@GetMapping("/order/{uid}")
-	public List<OrderDTO> getAllOrders(@PathVariable("uid") Long uid){
+	public List<OrderDTO> getAllOrders(@PathVariable("uid") Long uid) {
 		return orderService.getAllOrders(uid);
-		
+
 	}
-	
+
+	@PostMapping("/order/{oid}")
+	public ResponseEntity<OrderDTO> getOrder(@PathVariable("oid") Long oid) {
+		try {
+			OrderDTO getOrder = orderService.getOrder(oid);
+			return ResponseEntity.ok(getOrder);
+		} catch (Exception ex) {
+			return ResponseEntity.badRequest().body(null);
+		}
+
+	}
+
 }
