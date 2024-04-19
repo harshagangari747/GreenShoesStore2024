44cc863b4cee18fb1a0b69cfbb1a3692e3fbc6d1
Sai Bharath Reddy Lattupalli <saibharathreddylattupalli@Sais-MacBook-Air.local>
2024-04-18 21:10:44 -0400

    returns products with less than 10 stock

--- GreenShoeStore/src/main/java/com/store/greenShoes/repository/ProductSizeColorRepository.java
+++ GreenShoeStore/src/main/java/com/store/greenShoes/repository/ProductSizeColorRepository.java
@@ -11,7 +11,7 @@ import com.store.greenShoes.model.ProductSizeColor;
 import com.store.greenShoes.model.Size;
 
 public interface ProductSizeColorRepository extends JpaRepository<ProductSizeColor, Long> {
-
+	
 	//List<ProductSizeColor> getByProductId(Long id);
 	
 	@Query("select psc from ProductSizeColor psc where psc.productId=?1 and psc.sizeId=?2")
@@ -21,6 +21,14 @@ public interface ProductSizeColorRepository extends JpaRepository<ProductSizeCol
 	List<ProductSizeColor> findByProduct(Product product);
 	@Query("select psc from ProductSizeColor psc where psc.productId=?1 and psc.sizeId=?2 and psc.colorId=?3" )
 	ProductSizeColor findByProductSizeColor(Product product, Size size, Color color);
+	
+	
+	@Query("SELECT psc.productId FROM ProductSizeColor psc JOIN (SELECT SUM(psc2.Quantity) AS totalQuantity, psc2.productId as productsId FROM ProductSizeColor psc2 GROUP BY psc2.productId) p ON psc.productId = p.productsId WHERE p.totalQuantity < 10 GROUP BY psc.productId")
+	
+
+	
+	//@Query("select psc.productId from ProductSizeColor psc Join (select sum(quantity) as quantity,productId from ProductSizeColor group by productId) p on psc.productId=p.product_id where p.quantity<400 group by psc.productId")
+	List<Product> getLowStock();
 
 	//List<ProductSizeColor> findByProductSize(Long id, Long id2);
 	
